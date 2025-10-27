55cc20d03fd641d52054de60bb1ae0243c2b0da6
damiannluczak <152283021+damiannluczak@users.noreply.github.com>
2025-10-27 17:40:01 +0100

    feat: add compiler skeleton with lexer/parser/codegen structure

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void lexer(){
+
+}
+
+void tokenize(){
+    // └─ next_token() (w pętli)
+}
+
+void parser(){
+//  │    ├─ parse_func()
+//  │    │    ├─ parse_block()
+ // │    │    │    └─ parse_stmt()
+ // │    │    │         └─ parse_expr()
+
+}
+
+void c_code_gen(){
+//└─ gen_node() → (rekurencja po AST)
+
+}
+
+void build_and_run(){ 
+    //gcc ... ""
+}
+
+
+int main () { 
+    printf("ogien");
+    return 0;
+}
